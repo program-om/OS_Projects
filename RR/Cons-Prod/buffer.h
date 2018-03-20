8eceba3c99d6aa193bc147c8431eea3f6ca49c33
program-om <az.s.sh94@gmail.com>
2018-03-20 00:56:15 -0500

    RR Project almost completed

--- RR/Cons-Prod/buffer.h
+++
@@ -1,4 +0,0 @@
-/* buffer.h */
-
-typedef int buffer_item;
-#define BUFFER_SIZE 5
--- RR/main.cpp
+++ RR/main.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <string>   //stoi
 #include <sstream>  //used for parsing the input
 #include <algorithm>//used for parsing the input
 #include <iterator> //used for parsing the input
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -15,14 +18,15 @@ struct PCB{
     int burstTime,
         quantumTime,
         PID;
-    int waitingEvent; //if the process waiting event, this is the number of the event
+    int waitingEvent = 0; //if the process waiting event, this is the number of the event
 };
 
-void Create(int n, int burstTime);
-void waitProcess(int n, PCB &runningProcess);
-void Destroy(int n);
-void terminateFamily(PCB parent);
-void Display();
+void Create(int n, int burstTime, ofstream &outFile, int q);
+void waitProcess(int n, PCB &runningProcess, ofstream &outFile);
+void Destroy(int n, ofstream &outFile);
+void terminateFamily(PCB parent, ofstream &outFile);
+void Display(ofstream &outFile);
+void Event(int n, ofstream &outFile);
 
 
 vector<PCB> ready; //ready queue
@@ -32,23 +36,40 @@ PCB runningProcess;
 PCB PID0;
 
 int main(){
-    bool exit = false;
-    int q = 3;
+    bool Exit = false;
     PID0.PID = 0;
     PID0.burstTime = 1000000;
+    PID0.quantumTime = 0;
+    string input;
+
+
+
+    ifstream inFile("input.txt");
+    ofstream outFile("output.txt");
+
+
+    if (!inFile)
+    {
+        cout << "ERROR: The input or output file could NOT openned!" << endl;
+        exit(0);
+    }
+
+
+    getline(inFile, input);
+    int q = stoi(input);
 
     runningProcess.quantumTime = 0;
+    runningProcess = PID0;
 
-    cout << "PID 0 running" << endl;
-    Display();
+    Display(outFile);
 
-    while(!exit){
+    while(!Exit){
         //if there is no process in ready queue, set PID 0 as running process
 
         //get the input from the user
-        string input;
-        getline(cin, input);
-        //istringstream iss(input);
+
+        getline(inFile, input);
+        outFile << input << endl;
         vector<string> args;
 
         std::stringstream iss(input);
@@ -58,39 +79,40 @@ int main(){
              back_inserter(args)
         );
 
-        // for(int i=0; i < args.size(); i++){
-        //     cout << i << " - " << args[i] << endl;
-        // }
-
-
         if(args[0] == "C"){
             int n = stoi(args[1]);
             int burstTime = stoi(args[2]);
-            Create(n, burstTime);
+            Create(n, burstTime, outFile, q);
         } else if(args[0] == "D"){
             int n = stoi(args[1]);
-            Destroy(n);
+            Destroy(n, outFile);
         } else if(args[0] == "I"){
 
         } else if(args[0] == "W"){
+            int n = stoi(args[1]);
+            waitProcess(n, runningProcess, outFile);
 
-        } else if(args[0] == "E"){
-
-        } else if(args[0] == "X"){
-            exit = true;
-        } else {
-            cout << "Invalid input!" << endl;
+        } else if(args[0] == "X") {
+            outFile << "Current state of simulation:" << endl;
+            Exit = true;
+            runningProcess.quantumTime++;
+            runningProcess.burstTime++;
+            Display(outFile);
+            break;
         }
+//        } else {
+//            cout << "Invalid input!" << endl;
+//        }
 
         if(runningProcess.burstTime == 0){
             //terminate
-            //call Destroy(runningProcess.PID)
+            Destroy(runningProcess.PID, outFile);
         }
 
         if(runningProcess.quantumTime == 0){
             if(runningProcess.PID != 0){
                 ready.push_back(runningProcess); //add this process to ready que
-                cout << "PID " << runningProcess.PID << " placed on Ready Queue" << endl;
+                outFile << "PID " << runningProcess.PID << " " << runningProcess.burstTime << " placed on Ready Queue" << endl;
             }
 
             if(!ready.empty()){
@@ -100,15 +122,22 @@ int main(){
             } else{
                 runningProcess = PID0;
             }
-            runningProcess.quantumTime = 3;
+            if(runningProcess.PID != 0)
+                runningProcess.quantumTime = q;
 
         }
 
-        if(runningProcess.PID != 0) {
-            cout << "PID " << runningProcess.PID << " " << runningProcess.burstTime
-                 << " running with " << runningProcess.quantumTime << " left" << endl;
+        if(args[0] == "E"){
+            int n = stoi(args[1]);
+            for(int i=0; i < EventWaitList.size(); i++){
+                if(EventWaitList[i] == n){
+                    Event(n, outFile);
+                }
+            }
+
         }
-        Display();
+
+        Display(outFile);
 
         if(runningProcess.quantumTime != 0){
             runningProcess.quantumTime--;
@@ -117,10 +146,12 @@ int main(){
 
     }
 
+    inFile.close();
+    outFile.close();
     return 0;
 }
 
-void Create(int n, int burstTime){
+void Create(int n, int burstTime, ofstream &outFile, int q){
 
     //create a new process
     PCB process;
@@ -129,82 +160,99 @@ void Create(int n, int burstTime){
     //assign burstTime to process burstTime
     process.burstTime = burstTime;
     //set the quntum time to 3 currently
-    process.quantumTime = 3;
+    process.quantumTime = q;
     //set the current running process to be the parent of the new process
     process.parent = &runningProcess;
     //set the new process as child to the running process
     runningProcess.child.push_back(process);
     //append the new process to ready queue
     ready.push_back(process);
-    cout << "PID " << process.PID << " placed on Ready Queue" << endl;
+    outFile << "PID " << process.PID << " " << process.burstTime << " placed on Ready Queue" << endl;
 
 }
 
-void Destroy(int n){
+void Destroy(int n, ofstream &outFile){
     //find the process n
     PCB process;
     //is it the running process
     if(runningProcess.PID == n){
         process = runningProcess;
-        terminateFamily(process);
+        terminateFamily(process, outFile);
         runningProcess = PID0; //make sure running process is destroyed ...
     }
     //is it in the waiting q
     for(int i=0; i < waiting.size(); i++){
         if(waiting[i].PID == n){
-            terminateFamily(waiting[i]);
+            terminateFamily(waiting[i], outFile);
             waiting.erase(waiting.begin()+i);
         }
     }
     //is it in the ready q
     for(int i=0; i < ready.size(); i++){
         if(ready[i].PID == n){
-            terminateFamily(ready[i]);
+            terminateFamily(ready[i], outFile);
             ready.erase(ready.begin()+i);
         }
     }
     //delete the children of process n
     //process.child.clear();
     //delete the process n
+    runningProcess = PID0;
 }
 
-void terminateFamily(PCB parent){
+void terminateFamily(PCB parent, ofstream &outFile){
 
-    cout << "PID " << parent.PID << " " << parent.burstTime << " terminated" << endl;
+    outFile << "PID " << parent.PID << " " << parent.burstTime << " terminated" << endl;
 
     for(int i=0; i < parent.child.size(); i++){
         //cout << "PID " << parent.child[i].PID << " " << parent.child[i].burstTime << " terminated" << endl;
-        Destroy(parent.child[i].PID);
+        Destroy(parent.child[i].PID, outFile);
     }
     parent.child.clear(); //termniate all children
 }
 
-void waitProcess(int n, PCB &runningProcess){
-    //move the runningProcess to wait que
-    waiting.push_back(runningProcess);
+void waitProcess(int n, PCB &runningProcess, ofstream &outFile){
     //set n to waitEvent
     runningProcess.waitingEvent = n;
+    //move the runningProcess to wait que
+    waiting.push_back(runningProcess);
+
     //add n to EventWait list
     EventWaitList.push_back(n);
-    //set the first element on the ready que as the runningProcess
-    runningProcess = ready[0];
+    //set PID0 as running process, if some process in ready it will be setted.
+    outFile << "PID " << runningProcess.PID << " " << runningProcess.burstTime << " placed on Wait Queue" << endl;
+    runningProcess = PID0;
+
 }
 
-// void Interrupt(){
-
-// }
-
-// void Event(){
-
-// }
-
-void Display(){
-    cout << "Ready Queue: ";
-    for(int i=0; i < ready.size(); i++){
-        cout << "PID" << ready[i].PID << " " << ready[i].burstTime << " ";
+void Event(int n, ofstream &outFile){
+    //move the element with n waitingEvent from waiting que to ready que
+    for(int i=0; waiting.size(); i++){
+        if(waiting[i].waitingEvent == n){
+            ready.push_back(waiting[i]);
+            waiting.erase(waiting.begin()+i);
+            outFile << "PID " << waiting[i].PID << " " << waiting[i].burstTime << " placed on Ready Queue" << endl;
+        }
     }
-    cout << endl;
-    cout << "Wait Queue: ";
-    cout << endl;
+}
+
+void Display(ofstream &outFile){
+    if(runningProcess.PID != 0) {
+        outFile << "PID " << runningProcess.PID << " " << runningProcess.burstTime
+                << " running with " << runningProcess.quantumTime << " left" << endl;
+    } else{
+        outFile << "PID 0 running" << endl;
+    }
+
+    outFile << "Ready Queue: ";
+    for(int i=0; i < ready.size(); i++){
+        outFile << "PID " << ready[i].PID << " " << ready[i].burstTime << " ";
+    }
+    outFile << endl;
+    outFile << "Wait Queue: ";
+    for(int i=0; i < waiting.size(); i++){
+        outFile << "PID " << waiting[i].PID << " " << waiting[i].burstTime << " " << waiting[i].waitingEvent;
+    }
+    outFile << endl;
 }
 
